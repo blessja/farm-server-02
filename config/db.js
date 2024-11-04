3f771d340190bb8dbbccf1cffc37f4e2682a9999
Blessing Jackson <jaydenj772@gmail.com>
2024-11-04 07:47:10 +0200

    uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(
+      "mongodb+srv://admin-Jackson:jaydenjackson1@cluster0.bnu3c.mongodb.net/farm-managment?retryWrites=true&w=majority"
+    );
     console.log("MongoDB connected!!");
   } catch (error) {
     console.log("Failed to connect to MongoDB", error);
