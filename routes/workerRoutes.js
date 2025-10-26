5a249bdca9a62b23b0e2c5262f1dcd0cb0915e4b
Blessing Jackson <jaydenj772@gmail.com>
2025-10-26 19:31:56 +0200

    add route to regular

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -8,4 +8,10 @@ router.post("/checkinworker", workerController.checkInWorkers);
 // Check-out a worker
 router.post("/checkoutworker", workerController.checkOutWorkers);
 
+// routes/workerRoutes.js - Add this route
+router.get(
+  "/regular-piecework-totals",
+  workerController.getRegularPieceworkTotals
+);
+
 module.exports = router;
